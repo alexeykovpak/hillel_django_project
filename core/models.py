22e8d50b5f8c643a6caf4279d34c60a1fb5747ec
Alexey Kovpak <alexeykovpak01@gmail.com>
2021-03-24 19:48:36 +0200

    Homework7 is done

--- core/models.py
+++ core/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.db.models.aggregates import Count, Avg, Max, Min
 
 # Create your models here.
+
+
+class GroupManager(models.Manager):
+    
+    def get_group_with_student_count(self):
+        return self.get_queryset().annotate(stq=Count('student'), stavg=Avg('student__age'), stmax=Max('student__age'), stmin=Min('student__age'))
 
 
 class Teacher(models.Model):
@@ -24,9 +31,12 @@ class Group(models.Model):
     def __str__(self):
         return self.name
 
+    objects = GroupManager()
+
 
 class Student(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False, unique=True)
+    age = models.PositiveSmallIntegerField()
     email = models.EmailField(max_length=50)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
--- core/views.py
+++ core/views.py
@@ -8,6 +8,12 @@ class GroupView(ListView):
     template_name = 'group_index.html'
     model = Group
 
+    def get_context_data(self, **kwargs):
+        context = super(GroupView, self).get_context_data(**kwargs)
+        groups = Group.objects.get_group_with_student_count().prefetch_related('teachers')
+        context['groups'] = groups
+        return context
+
 
 class TeacherView(ListView):
     template_name = 'teacher_index.html'
